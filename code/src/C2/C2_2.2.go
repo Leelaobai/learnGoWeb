23185521205385bf788d225e942b0c48ad2515a8
lidh2 <lidh2@cdfortis.com>
2019-06-05 18:05:02 +0800

    提交190605日工作产出

--- code/src/C2/C2_2.2.go
+++ code/src/C2/C2_2.2.go
@@ -11,7 +11,7 @@ func HelloC2() {
 func DefineVar() {
 	var v1 int
 	v1 = 1
-	var v2,v3,v4 int
+	var v2, v3, v4 int
 	v2 = 2
 	v3 = 3
 	v4 = 4
@@ -19,8 +19,8 @@ func DefineVar() {
 	var v6, v7, v8 int = 6, 7, 8
 	var v9, v10, v11 = 9, 10, 11
 	v12, v13, v14 := 12, 13, 14
-	
-	fmt.Println(v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12,v13,v14)
+
+	fmt.Println(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14)
 }
 
 //定义常量
@@ -30,14 +30,14 @@ func DefineVar() {
 //语法：	const constantName (type) = value
 func Define_const() {
 	const PI float32 = 3.1415926
-	
+
 	const i = 10000
-	
+
 	const MaxThread = 10
-	
+
 	const prefix = "astaxie_"
-	
-	fmt.Println(PI,i,MaxThread,prefix)
+
+	fmt.Println(PI, i, MaxThread, prefix)
 }
 
 //GO内置基础类型
@@ -45,20 +45,20 @@ func Define_const() {
 //boolean：布尔类型
 
 func Test_boolean() {
-	var isActive bool //一般声明
+	var isActive bool                   //一般声明
 	var enabled, disabled = true, false //忽略类型的生命
-	var available bool //一般声明
-	valid := false //简短声明
-	available = true //赋值操作
-	
-	fmt.Println(isActive,enabled,disabled,available,valid)
+	var available bool                  //一般声明
+	valid := false                      //简短声明
+	available = true                    //赋值操作
+
+	fmt.Println(isActive, enabled, disabled, available, valid)
 }
 
 //数值类型
 
 func Test_number() {
-	var c complex64 = 5+5i
-	fmt.Printf("%v",c)
+	var c complex64 = 5 + 5i
+	fmt.Printf("%v", c)
 }
 
 //字符串类型
@@ -69,23 +69,23 @@ func Test_string() {
 	no, yes, maybe := "no", "yes", "maybe"
 	japaneseHello := "Ohaiou"
 	frenchHello = "Bonjour"
-	
+
 	s := "hello"
-	c := []byte(s)	//将字符串转换为[]byte类型
+	c := []byte(s) //将字符串转换为[]byte类型
 	c[0] = 'c'
 	s2 := string(c)
-	
+
 	ss := "hello"
 	hh := " world!"
 	sh := ss + hh
-	
+
 	sss := "hahaha"
 	sss1 := sss[1:]
 	sss2 := "c" + sss1
-	
+
 	m := `hello		
 			world`
-	
+
 	fmt.Println(frenchHello, emptyString, japaneseHello, s, c, s2, no, yes, maybe, sh, sss, sss2, reflect.TypeOf(sss1))
 	fmt.Println(m)
 }
@@ -95,13 +95,13 @@ func Test_string() {
 //iota枚举
 
 func Test_iota() {
-	const(
+	const (
 		a = iota
 		b = iota
 		_
 		d
 	)
-	fmt.Println(a,b,d)
+	fmt.Println(a, b, d)
 }
 
 //数组
@@ -110,14 +110,14 @@ func Test_array() {
 	var arr [10]int
 	arr[0] = 42
 	arr[1] = 13
-	a := [3]int{1,2,3}
-	b := [10]int{1,2,3}
-	c := [...]int{4,5,6}
-	doubleArray := [2][4]int{[4]int{1,2,3,4},[4]int{5,6,7,8}}
-	
-	esayArray := [2][4]int{{1,2,3,4},{5,6,7,8}}
-	
-	fmt.Println(arr,a,b,c,doubleArray,esayArray)
+	a := [3]int{1, 2, 3}
+	b := [10]int{1, 2, 3}
+	c := [...]int{4, 5, 6}
+	doubleArray := [2][4]int{[4]int{1, 2, 3, 4}, [4]int{5, 6, 7, 8}}
+
+	esayArray := [2][4]int{{1, 2, 3, 4}, {5, 6, 7, 8}}
+
+	fmt.Println(arr, a, b, c, doubleArray, esayArray)
 }
 
 //切片
@@ -126,22 +126,22 @@ func Test_array() {
 func Test_slice(){
 	var fslice []int
 	slice := []byte{'a','b','c','d'}
-	
+
 	var ar = [10]byte{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'}
-	
+
 	var a,b []byte
-	
+
 	a = ar[2:5]
-	b = ar[3:5]	
+	b = ar[3:5]
 }
 */
 
 //条件判断语句
 
 func Test_ifelse() {
-	if x:=10;x<10 {
+	if x := 10; x < 10 {
 		fmt.Println("这数小于10！")
-	}else{
+	} else {
 		fmt.Println("这数大于等于10！")
 	}
 }
@@ -150,9 +150,72 @@ func Test_ifelse() {
 
 func Test_range_map() {
 	m := make(map[string]int)
-	m["one"]=1
-	m["two"]=2
-	for k,v:=range m{
-		fmt.Println("key：",k,"value：",v)
+	m["one"] = 1
+	m["two"] = 2
+	for k, v := range m {
+		fmt.Println("key：", k, "value：", v)
 	}
+}
+
+//函数作为值；类型
+//在go语言中函数也是一种变量，它的类型就是拥有相同参数相同返回值的一种类型。
+func IsOdd(integer int) bool {
+	if integer%2 == 0 {
+		return false
+	}
+	return true
+}
+
+func IsEven(integer int) bool {
+	if integer%2 == 0 {
+		return true
+	}
+	return false
+}
+
+//结构体
+type Person struct {
+	Name string
+	Age  int
+}
+
+func Older(p1, p2 Person) (Person, int) {
+	if p1.Age > p2.Age {
+		return p1, p1.Age - p2.Age
+	}
+	return p2, p2.Age - p1.Age
+}
+
+//方法的继承
+
+type Human struct {
+	name  string
+	age   int
+	phone string
+}
+type Student struct {
+	Human  //匿名字段
+	school string
+}
+type Employee struct {
+	Human   //匿名字段
+	company string
+}
+
+//Human定义method
+func (h *Human) SayHi() {
+	fmt.Printf("Hi, I am %s you can call me on %s\n", h.name, h.phone)
+}
+
+//Employee的method重写Human的method
+func (e *Employee) SayHi() {
+	fmt.Printf("Hi, I am %s, I work at %s. Call me on %s\n", e.name,
+		e.company, e.phone) //Yes you can split into 2 lines here.
+}
+func TestMethod() {
+	mark := Student{Human{"Mark", 25, "222-222-YYYY"}, "MIT"}
+	sam := Employee{Human{"Sam", 45, "111-888-XXXX"}, "Golang Inc"}
+	mark.SayHi()
+	sam.SayHi()
+	sam.Human.SayHi()
 }
--- code/src/main/main.go
+++ code/src/main/main.go
@@ -2,7 +2,49 @@ package main
 
 import (
 	"C2"
+	"fmt"
 )
+
+type testInt func(int) bool
+
+func filter(slice []int, f testInt) []int {
+	var result []int
+	for _, value := range slice {
+		if f(value) {
+			result = append(result, value)
+		}
+	}
+	return result
+}
+
+func testFilter() {
+	slice := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	fmt.Println("slice = ", slice)
+	odd := filter(slice, C2.IsOdd)
+	fmt.Println("Odd elements of slice are: ", odd)
+	even := filter(slice, C2.IsEven)
+	fmt.Println("Even elements of slice are: ", even)
+}
+
+func testStruct() {
+	var tom C2.Person
+	tom.Name, tom.Age = "Tom", 18
+
+	bob := C2.Person{Age: 25, Name: "Bob"}
+
+	paul := C2.Person{"Paul", 43}
+
+	tb_Older, tb_diff := C2.Older(tom, bob)
+	tp_Older, tp_diff := C2.Older(tom, paul)
+	bp_Older, bp_diff := C2.Older(bob, paul)
+	fmt.Printf("Of %s and %s, %s is older by %d years\n",
+		tom.Name, bob.Name, tb_Older.Name, tb_diff)
+	fmt.Printf("Of %s and %s, %s is older by %d years\n",
+		tom.Name, paul.Name, tp_Older.Name, tp_diff)
+	fmt.Printf("Of %s and %s, %s is older by %d years\n",
+		bob.Name, paul.Name, bp_Older.Name, bp_diff)
+
+}
 
 func main() {
 	//C2.HelloC2()
@@ -13,5 +55,7 @@ func main() {
 	//C2.Test_iota()
 	//C2.Test_array()
 	//C2.Test_ifelse()
-	C2.Test_range_map()
+	//C2.Test_range_map()
+	//testStruct()
+	C2.TestMethod()
 }
